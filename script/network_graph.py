954ca49edfca1da274f3703e83192ea0e2b7f641
Matieu Allier <matieu.allier@gmail.com>
2021-08-07 01:03:16 +0200

    Creation of a function to create the graph and new info linked to the color

--- script/network_graph.py
+++ script/network_graph.py
@@ -4,29 +4,38 @@ import matplotlib.pyplot as plt
 
 from github_export import get_repository_tree, format_tree_df
 
-def add_nodes_pyvis(g, dict_graph_tree):
-    nodes = list(dict_graph_tree.keys())
-    nodes += []
-    print(nodes)
-    nodes_labels = nodes
+color_of_extension = {
+    "py" : "#3572A5",
+    "js" : "#f1e05a",
+    "css" : "#563d7c",
+    "html" : "#e34c26",
+    "json" : "#292929",
+    "folder" : "dark-grey",
+}
 
-if __name__ == '__main__':
-    owner = "octocat"
-    rep = "octocat.github.io"
-    branch = "master"
-    tree_data = get_repository_tree(owner, rep, branch="master")
-    df_graph_tree = format_tree_df(tree_data)
-    net = Network(height='750px', width='100%')
-    net.barnes_hut()
+def create_network_graph(df_graph_tree):
+    """Create a Network from the database of nodes and edges"""
+    net = Network(height='750px', width='100%', directed=True)
+    net.force_atlas_2based()
     for index, row in df_graph_tree.iterrows():
         src = row['Source']
         dst = row['Target']
         label = row['Label']
-        print(label)
-        net.add_node(src, label=label)
+        title = "File fullname : {} <br> Type : {}".format(row['Source'], row['File Type'])
+        color = color_of_extension[row['File Type']] if row['File Type'] in color_of_extension.keys() else 'grey'
+        net.add_node(src, label=label, color = color, title = title)
         if dst != '':
-            net.add_node(dst, label=label)
-            net.add_edge(src, dst, value=1)
-    # set the physics layout of the network
+            net.add_node(dst, label=label, )
+            net.add_edge(src, dst, value=1, color = '#ADD8E6')
+    return net
     
+if __name__ == '__main__':
+    owner = "facebook"
+    rep = "prophet"
+    branch = "master"
+    tree_data = get_repository_tree(owner, rep, branch="master")
+    df_graph_tree = format_tree_df(tree_data)
+    net = create_network_graph(df_graph_tree)
+    # set the physics layout of the network
+    #net.show_buttons(filter_=['nodes'])
     net.show('test.html')
