a9280780817b76bd975032d70ea5c40a4ea65242
Matieu Allier <matieu.allier@gmail.com>
2021-08-07 16:38:24 +0200

    Change in the Color of the graph

--- script/network_graph.py
+++ script/network_graph.py
@@ -11,24 +11,26 @@ color_of_extension = {
     "html" : "#e34c26",
     "json" : "#292929",
     "folder" : "light grey",
+    "r" : "#198CE7",
+    "rd" : "#198CE7",
 }
 
 def create_network_graph(df_graph_tree):
     """Create a Network from the database of nodes and edges"""
-    net = Network(height='750px', width='100%', directed=True)
+    net = Network(height='750px', width='100%', directed=True, bgcolor='#222222', font_color='white')
     net.force_atlas_2based()
     for index, row in df_graph_tree.iterrows():
         src = row['Source']
         dst = row['Target']
         label = row['Label']
         title = "File fullname : {} <br> Type : {}".format(row['Source'], row['File Type'])
-        color = color_of_extension[row['File Type']] if row['File Type'] in color_of_extension.keys() else 'grey'
+        color = color_of_extension[row['File Type'].lower()] if row['File Type'].lower() in color_of_extension.keys() else 'grey'
         if row['File Type'] == 'folder':
-            net.add_node(src, shape='diamond', label=label, color = color, title = title)
+            net.add_node(src, shape='text', label=label, color = color, title = title)
         else:
             net.add_node(src, shape='dot', label=label, color = color, title = title)
         if dst != '':
             #net.add_node(dst, label=label, title=title)
-            net.add_edge(src, dst, value=1, color = '#ADD8E6')
+            net.add_edge(src, dst, value=1, color = '#d3d3d3')
     return net
     
